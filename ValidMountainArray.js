c73fdb4a7ca90ccb72539c339c39d030a1c47cd5
Chandan Dhamande <virtualassistance.py@gmail.com>
2021-05-21 16:01:47 +0530

    Leetcode validMountainarray (fast)

--- ValidMountainArray.js
+++ ValidMountainArray.js
@@ -1,34 +1,15 @@
 var validMountainArray = function(arr) {
-     let ans = false;
-     var maxIndex = 0;
-     if (arr.length > 2) {
-     	for (let i = 0; i < arr.length - 1; i++) {
-     		if (arr[i] < arr[i + 1]) {
-     			maxIndex = i + 1;
-     		}
-     		else {
-     			break;
-     		}
-     	}
-     	if (maxIndex === 0 || maxIndex === arr.length - 1) {
-     		return ans;
-     	} 
-     	else {
-     		for (let j = maxIndex; j < arr.length - 1; j++) {
-     			if (arr[j + 1] < arr[j]) {
-     				ans = true;
-     			}
-     			else {
-     				ans = false
-     				break;
-     			}
-     		}
-     	}
-     	return ans;
+     let maxIndex = 0;
+     while (arr[maxIndex] < arr[maxIndex + 1]) {
+     	maxIndex++;
      }
-     else {
-     	return ans;
-     } 
+     if (maxIndex === 0 || maxIndex === arr.length - 1) {
+     	return false;
+     }
+     while (arr[maxIndex] > arr[maxIndex + 1]) {
+     	maxIndex++;
+     }
+     return maxIndex === arr.length - 1 && arr.length > 2;  // returns a boolean value	
 };
 
 console.log(validMountainArray([1, 3, 2]));

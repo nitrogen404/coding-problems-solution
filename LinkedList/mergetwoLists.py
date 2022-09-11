e501ee97b512afde3af46b9eebc0802652f75dc9
nitrogen404 <virtualassistance.py@gmail.com>
2022-09-11 18:19:10 +0530

    merge two sorted Linkedlists

---
+++ LinkedList/mergetwoLists.py
@@ -0,0 +1,26 @@
+# O(n + m) time 
+# O(1) space
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def mergeLinkedLists(headOne, headTwo):
+    p1 = headOne
+    p2 = headTwo
+    prev = None
+    while p1 is not None and p2 is not None:
+        if p1.value < p2.value:
+            prev = p1
+            p1 = p1.next
+        else:
+            if prev is not None:
+                prev.next = p2
+            prev = p2
+            p2 = p2.next
+            prev.next = p1
+    if p1 is None:
+        prev.next = p2
+    return headOne if headOne.value < headTwo.value else headTwo

3a74305d5cf83a215c1f6b162d9ba07cbba130fa
nitrogen404 <virtualassistance.py@gmail.com>
2022-11-04 10:10:02 +0530

    is monotonic array

---
+++ arrays/monotonicArray.py
@@ -0,0 +1,20 @@
+# O(n) time 
+# O(1) space
+def isMonotonic(array):
+    if len(array) <= 2:
+        return True
+
+    direction = array[1] - array[0]
+    for i in range(2, len(array)):
+        if direction == 0:
+            direction = array[i] - array[i - 1]
+            continue
+        if breaksDirection(direction, array[i - 1], array[i]):
+            return False
+    return True
+
+def breaksDirection(direction, prevInt, currentInt):
+    difference = currentInt - prevInt
+    if direction > 0:
+        return difference < 0
+    return difference > 0

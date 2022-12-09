08553198d11ebf68c9eb89aa13c7909a56a94b65
nitrogen404 <virtualassistance.py@gmail.com>
2022-12-09 21:38:09 +0530

    search in sorted rotated array

---
+++ arrays/searchinRotatedArray.py
@@ -0,0 +1,20 @@
+# O(logn) time 
+# O(1) space
+def search(self, nums: List[int], target: int) -> int:
+        low = 0 
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[low] <= nums[mid]: # if left part is sorted
+                if nums[low] <= target and target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1

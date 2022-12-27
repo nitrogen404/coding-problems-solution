2d358e79758f4dd345179c0e71c06771e38033ae
nitrogen404 <virtualassistance.py@gmail.com>
2022-12-27 11:46:24 +0530

    merge sorted arrays O(1) space

--- arrays/mergesortedArrays.py
+++ arrays/mergesortedArrays.py
@@ -1,20 +1,29 @@
-# O(m) time
+# O(m) where m is the length of big array time
 # O(1) space
-
+ 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        bigPointer = 0
-        smallPointer = 0
-        for i in range(len(nums2)):
-            if nums1[bigPointer] < nums2[smallPointer]:
-                bigPointer += 1
+        i = m - 1
+        j = n - 1
+        k = (m + n) - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
             else:
-                nums1[bigPointer], nums2[smallPointer] = nums2[smallPointer], nums1[bigPointer]
-                bigPointer += 1
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
 
-        for i in range(len(nums2)):
-            nums1[bigPointer] = nums2[i]
-            bigPointer += 1
+        while i >= 0:
+            nums1[k] = nums1[i]
+            k -= 1
+            i -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1

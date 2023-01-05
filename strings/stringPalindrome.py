6fd24a2c1d4af0a8022820d69a04abcb80b3e988
nitrogen404 <virtualassistance.py@gmail.com>
2023-01-05 16:50:10 +0530

    string palindrome

---
+++ strings/stringPalindrome.py
@@ -0,0 +1,12 @@
+# O(n) time 
+# O(1) space
+
+def isPalindrome(string):
+    leftPointer = 0
+    rightPointer = len(string) - 1
+    while leftPointer < rightPointer:
+        if string[leftPointer] != string[rightPointer]:
+            return False
+        leftPointer += 1
+        rightPointer -= 1
+    return True
